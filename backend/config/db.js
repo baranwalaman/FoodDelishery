2847a0aaead3cc81fdc8636f2a6f811f08286298
Aman Baranwal <amanbaranwal2017@gmail.com>
2024-11-15 22:50:58 +0530

    backend completed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://amanbaranwal2017:fooddel@fooddel.oph54.mongodb.net/').then(()=>console.log("DB Connected")).catch((error) => console.log("DB Connection Error:", error));
+    await mongoose.connect('mongodb+srv://amanbaranwal2017:fooddelishery@fooddel.oph54.mongodb.net/food-del').then(()=>console.log("DB Connected")).catch((error) => console.log("DB Connection Error:", error));
 }
